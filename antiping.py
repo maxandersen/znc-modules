5292a49e8f5c8fe78918f8d3938c26218a7dda8b
Max Rydahl Andersen <max.andersen@gmail.com>
2015-04-20 00:56:33 +0200

    Update antiping.py
    nicer response message and dont reply to empty pings in channel without your nick mentioned.

--- antiping.py
+++ antiping.py
@@ -14,27 +14,36 @@ class antiping(znc.Module):
         self.process(nick, None, message)
         return znc.CONTINUE
 
-    def isAntiPing(self, message):
+    def removeNoop(self, text):
+       chars = ":.!? "
+       for c in chars:
+         if c in text:
+            text = text.replace(c, "")
+       return text
+
+    def isPrivAntiPing(self, message):
         raw = message.s
         # remove current nick to handle messages that mention me directly
         raw = raw.replace(self.GetNetwork().GetCurNick(),'')
-        # remove common 'noop' characters
-        raw = raw.replace(':','')
-        raw = raw.replace('?','')
-        raw = raw.replace('!','')
-        raw = raw.replace(' ','')
+
+        raw = self.removeNoop(raw)
 
         if self.directantiping.match(raw):
            return True
         else:
            return False
 
+    def isChannelAntiPing(self,message):
+        return self.GetNetwork().GetCurNick() in message.s and self.isPrivAntiPing(message)
+
     def process(self, nick, channel, message):
-        if self.isAntiPing(message):
-           if channel is None:
+        self.PutModule('Blah')
+        if channel is None:
+           if self.isPrivAntiPing(message):
               self.PutModule('Antiping from {0}: "{1}"'.format(nick.GetNick(),message.s))
-              self.PutIRC('PRIVMSG {0} : (auto-antiping) Please do not just send an empty ping to me. Ask the question you have and I will reply ASAP. Thanks.'.format(nick.GetNick())) 
-           else:       
+              self.PutIRC('PRIVMSG {0} : (auto-antiping) Thanks for pinging me, but please just ask the question you have and I will reply ASAP. Thanks.'.format(nick.GetNick()))
+        else:
+           if self.isChannelAntiPing(message):
               self.PutModule('Antiping from {0} in {1}: {2}'.format(nick.GetNick(),channel,message.s))
-              self.PutIRC('PRIVMSG {0} : (auto-antiping) Please do not just send an empty ping in {1}. Ask the question you have and I will reply ASAP. Thanks.'.format(nick.GetNick(),channel)) 
+              self.PutIRC('PRIVMSG {0} : (auto-antiping) Please do not just send an empty ping in {1}. Ask the question you have and I will reply ASAP. Thanks.'.format(nick.GetNick(),channel))
               
